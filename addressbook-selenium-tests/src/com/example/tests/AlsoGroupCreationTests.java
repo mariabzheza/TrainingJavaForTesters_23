9b09b96dae0fc6099ef6cf79f0a34e2c623304e9
mariabzheza <maria.bzheza@gmail.com>
2015-04-21 16:34:49 +0300

    Home Work 1

---
+++ addressbook-selenium-tests/src/com/example/tests/AlsoGroupCreationTests.java
@@ -0,0 +1,29 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+public class AlsoGroupCreationTests extends TestBase {
+  @Test
+  public void testNonEmptyGroupGreation() throws Exception {
+	openMainPage();
+    gotoGroupPage();
+    initGroupCreation();
+    GroupData group = new GroupData();
+    group.name ="group name 1";
+    group.header = "header 1";
+    group.footer = "footer 1";
+	fillGroupForm(group);
+    submitGroupCreation();
+    returnToGroupsPage();
+  }
+  
+  @Test
+  public void testEmptyGroupGreation() throws Exception {
+	openMainPage();
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("", "", ""));
+    submitGroupCreation();
+    returnToGroupsPage();
+  }
+}
